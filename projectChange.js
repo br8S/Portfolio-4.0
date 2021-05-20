7fd2146111754983601cef8c14aa3fa30b830974
br8S <56180503+br8S@users.noreply.github.com>
2021-05-20 16:07:51 -0700

    new font for projects and got opacity & visibility to work on hover

--- projectChange.js
+++ projectChange.js
@@ -1,39 +1,50 @@
 document.querySelector('#project_1').addEventListener("mouseenter", () => {
     document.querySelector('#cacti_bg').style.visibility = "visible";
-    document.querySelector('#cacti_bg').classList.add("fade-background");
+    document.querySelector('#cacti_bg').classList.add("opacity-effect");
+})
+
+document.querySelector('#project_1').addEventListener("mouseleave", () => {
+    document.querySelector('#cacti_bg').style.visibility = "hidden";
+    document.querySelector('#cacti_bg').classList.remove("opacity-effect");
 })
 
 document.querySelector('#project_2').addEventListener("mouseover", () => {
     document.querySelector('#stussy_bg').style.visibility = "visible";
-    document.querySelector('#stussy_bg').classList.add("fade-background");
+    document.querySelector('#stussy_bg').classList.add("opacity-effect");
+})
+
+document.querySelector('#project_2').addEventListener("mouseleave", () => {
+    document.querySelector('#stussy_bg').style.visibility = "hidden";
+    document.querySelector('#stussy_bg').classList.remove("opacity-effect");
 })
 
 document.querySelector('#project_3').addEventListener("mouseover", () => {
     document.querySelector('#ballastpt_bg').style.visibility = "visible";
-    document.querySelector('#ballastpt_bg').classList.add("fade-background");
+    document.querySelector('#ballastpt_bg').classList.add("opacity-effect");
 })
+
+document.querySelector('#project_3').addEventListener("mouseleave", () => {
+    document.querySelector('#ballastpt_bg').style.visibility = "hidden";
+    document.querySelector('#ballastpt_bg').classList.remove("opacity-effect");
+})
+
 
 document.querySelector('#project_4').addEventListener("mouseover", () => {
     document.querySelector('#wedding_bg').style.visibility = "visible";
-    document.querySelector('#wedding_bg').classList.add("fade-background");
+    document.querySelector('#wedding_bg').classList.add("opacity-effect");
+})
+
+document.querySelector('#project_4').addEventListener("mouseleave", () => {
+    document.querySelector('#wedding_bg').style.visibility = "hidden";
+    document.querySelector('#wedding_bg').classList.remove("opacity-effect");
 })
 
 document.querySelector('#project_5').addEventListener("mouseover", () => {
     document.querySelector('#br8s_bg').style.visibility = "visible";
-    document.querySelector('#br8s_bg').classList.add("fade-background");
+    document.querySelector('#br8s_bg').classList.add("opacity-effect");
 })
 
-const projects = document.querySelectorAll('.project-item');
-
-for (const project of projects) {
-    project.addEventListener("mouseleave", function(event) {
-        document.querySelectorAll('.background').forEach( background => {
-            background.style.opacity = "0%";
-            document.querySelector('#cacti_bg').style.visibility = "hidden";
-            document.querySelector('#stussy_bg').style.visibility = "hidden";
-            document.querySelector('#ballastpt_bg').style.visibility = "hidden";
-            document.querySelector('#wedding_bg').style.visibility = "hidden";
-            document.querySelector('#br8s_bg').style.visibility = "hidden";
-        });
-    })
-}
+document.querySelector('#project_5').addEventListener("mouseleave", () => {
+    document.querySelector('#br8s_bg').style.visibility = "hidden";
+    document.querySelector('#br8s_bg').classList.remove("opacity-effect");
+})
