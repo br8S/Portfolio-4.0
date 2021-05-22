7cde7911a7ab7c83b2855ca5ebb10c8e3700fc97
br8S <56180503+br8S@users.noreply.github.com>
2021-05-22 14:37:17 -0700

    added on load animation, redid all animations using gsap

--- loadAnime.js
+++ loadAnime.js
@@ -5,4 +5,20 @@ window.addEventListener('load', function() {
         ease: Expo.easeInOut,
         delay: 4.6
     })
+
+    gsap.from(".project-item", {
+        duration: 4.2,
+        opacity: 0,
+        y: 1000,
+        ease: Expo.easeInOut,
+        delay: 5
+    })
+
+    gsap.from(".title", {
+        duration: 4.8,
+        opacity: 0,
+        y: 30,
+        ease: Expo.easeInOut,
+        delay: 5.4
+    })
 })
--- projectChange.js
+++ projectChange.js
@@ -1,51 +1,197 @@
 document.querySelector('#project_1').addEventListener("mouseenter", () => {
-    document.querySelector('#cacti_bg').style.visibility = "visible";
-    document.querySelector('#cacti_bg').classList.add("opacity-effect");
-    console.log(document.querySelector('#cacti_bg').style.opacity)
+    gsap.to("#project_1", {
+        duration: 0.1,
+        opacity: 1,
+        ease: Expo.easeIn
+    })
+
+    gsap.to("#cacti_bg", {
+        duration: 0.5,
+        autoAlpha: 1,
+        ease: Expo.easeInOut
+    })
 })
 
 document.querySelector('#project_1').addEventListener("mouseleave", () => {
-    document.querySelector('#cacti_bg').style.visibility = "hidden";
-    document.querySelector('#cacti_bg').classList.remove("opacity-effect");
+    gsap.to("#project_1", {
+        duration: 0.1,
+        opacity: 0.1,
+        ease: Expo.easeInOut
+    })
+
+    gsap.to("#cacti_bg", {
+        duration: 0.5,
+        autoAlpha: 0,
+        ease: Expo.easeInOut
+    })
 })
 
-document.querySelector('#project_2').addEventListener("mouseover", () => {
-    document.querySelector('#stussy_bg').style.visibility = "visible";
-    document.querySelector('#stussy_bg').classList.add("opacity-effect");
+document.querySelector('#project_2').addEventListener("mouseenter", () => {
+    gsap.to("#project_2", {
+        duration: 0.1,
+        opacity: 1,
+        ease: Expo.easeInOut
+    })
+
+    gsap.to("#stussy_bg", {
+        duration: 0.5,
+        autoAlpha: 1,
+        ease: Expo.easeInOut
+    })
 })
 
 document.querySelector('#project_2').addEventListener("mouseleave", () => {
-    document.querySelector('#stussy_bg').style.visibility = "hidden";
-    document.querySelector('#stussy_bg').classList.remove("opacity-effect");
+    gsap.to("#project_2", {
+        duration: 0.1,
+        opacity: 0.1,
+        ease: Expo.easeInOut
+    })
+
+    gsap.to("#stussy_bg", {
+        duration: 0.5,
+        autoAlpha: 0,
+        ease: Expo.easeInOut
+    })
 })
 
-document.querySelector('#project_3').addEventListener("mouseover", () => {
-    document.querySelector('#ballastpt_bg').style.visibility = "visible";
-    document.querySelector('#ballastpt_bg').classList.add("opacity-effect");
+document.querySelector('#project_3').addEventListener("mouseenter", () => {
+    gsap.to("#project_3", {
+        duration: 0.1,
+        opacity: 1,
+        ease: Expo.easeInOut
+    })
+
+    gsap.to("#ballast_bg", {
+        duration: 0.5,
+        autoAlpha: 1,
+        ease: Expo.easeInOut
+    })
 })
 
 document.querySelector('#project_3').addEventListener("mouseleave", () => {
-    document.querySelector('#ballastpt_bg').style.visibility = "hidden";
-    document.querySelector('#ballastpt_bg').classList.remove("opacity-effect");
+    gsap.to("#project_3", {
+        duration: 0.1,
+        opacity: 0.1,
+        ease: Expo.easeInOut
+    })
+
+    gsap.to("#ballast_bg", {
+        duration: 0.5,
+        autoAlpha: 0,
+        ease: Expo.easeInOut
+    })
 })
 
 
-document.querySelector('#project_4').addEventListener("mouseover", () => {
-    document.querySelector('#wedding_bg').style.visibility = "visible";
-    document.querySelector('#wedding_bg').classList.add("opacity-effect");
+document.querySelector('#project_4').addEventListener("mouseenter", () => {
+    gsap.to("#project_4", {
+        duration: 0.1,
+        opacity: 1,
+        ease: Expo.easeInOut
+    })
+
+    gsap.to("#wedding_bg", {
+        duration: 0.5,
+        autoAlpha: 1,
+        ease: Expo.easeInOut
+    })
 })
 
 document.querySelector('#project_4').addEventListener("mouseleave", () => {
-    document.querySelector('#wedding_bg').style.visibility = "hidden";
-    document.querySelector('#wedding_bg').classList.remove("opacity-effect");
+    gsap.to("#project_4", {
+        duration: 0.1,
+        opacity: 0.1,
+        ease: Expo.easeInOut
+    })
+
+    gsap.to("#wedding_bg", {
+        duration: 0.5,
+        autoAlpha: 0,
+        ease: Expo.easeInOut
+    })
 })
 
-document.querySelector('#project_5').addEventListener("mouseover", () => {
-    document.querySelector('#br8s_bg').style.visibility = "visible";
-    document.querySelector('#br8s_bg').classList.add("opacity-effect");
+document.querySelector('#project_5').addEventListener("mouseenter", () => {
+    gsap.to("#project_5", {
+        duration: 0.1,
+        opacity: 1,
+        ease: Expo.easeInOut
+    })
+
+    gsap.to("#br8s_bg", {
+        duration: 0.5,
+        autoAlpha: 1,
+        ease: Expo.easeInOut
+    })
 })
 
 document.querySelector('#project_5').addEventListener("mouseleave", () => {
-    document.querySelector('#br8s_bg').style.visibility = "hidden";
-    document.querySelector('#br8s_bg').classList.remove("opacity-effect");
+    gsap.to("#project_5", {
+        duration: 0.1,
+        opacity: 0.1,
+        ease: Expo.easeInOut
+    })
+
+    gsap.to("#br8s_bg", {
+        duration: 0.5,
+        autoAlpha: 0,
+        ease: Expo.easeInOut
+    })
+})
+
+window.addEventListener('load', function() {
+    gsap.to(".wrapper", {
+        duration: 4,
+        y: -1000,
+        ease: Expo.easeInOut,
+        delay: 4.6
+    })
+
+    gsap.from("#project_1", {
+        duration: 4.2,
+        opacity: 0,
+        x: 300,
+        ease: Expo.easeInOut,
+        delay: 5
+    })
+
+    gsap.from("#project_2", {
+        duration: 4.2,
+        opacity: 0,
+        x: 300,
+        ease: Expo.easeInOut,
+        delay: 5.2
+    })
+
+    gsap.from("#project_3", {
+        duration: 4.2,
+        opacity: 0,
+        x: 300,
+        ease: Expo.easeInOut,
+        delay: 5.4
+    })
+
+    gsap.from("#project_4", {
+        duration: 4.2,
+        opacity: 0,
+        x: 300,
+        ease: Expo.easeInOut,
+        delay: 5.6
+    })
+
+    gsap.from("#project_5", {
+        duration: 4.2,
+        opacity: 0,
+        x: 300,
+        ease: Expo.easeInOut,
+        delay: 5.8
+    })
+
+    gsap.from(".title", {
+        duration: 4.8,
+        opacity: 0,
+        y: 30,
+        ease: Expo.easeInOut,
+        delay: 6
+    })
 })
