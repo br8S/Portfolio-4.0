b245fb3e040114ba69f8095b0e337e67d0673411
br8S <56180503+br8S@users.noreply.github.com>
2021-05-26 13:09:26 -0700

    made changes to about page animation

--- logoHover.js
+++ logoHover.js
@@ -1,7 +1,8 @@
 document.querySelector("#logo").addEventListener("mouseenter", () => {
     gsap.to("#logo", {
         opacity: 1,
-        rotation: 70, 
+        scaleY: 1.05,
+        scaleX: 1.05,
         duration: 1
     })
 })
@@ -9,6 +10,8 @@ document.querySelector("#logo").addEventListener("mouseenter", () => {
 document.querySelector("#logo").addEventListener("mouseleave", () => {
     gsap.to("#logo", {
         opacity: 0.1,
+        scaleY: 1,
+        scaleX: 1,
         rotation: 0, 
         duration: 1
     })
@@ -19,5 +22,37 @@ document.querySelector("#logo").addEventListener("click", () => {
         x: 0,
         rotation: 0, 
         duration: 1
+    })
+
+    gsap.to("#logo", {
+        x: 400,
+        rotation: 0, 
+        duration: 1
+    })
+
+    gsap.from('.about-text', {
+        opacity: 0, 
+        duration: 2,
+        delay: 1
+    })
+
+    gsap.from('.social-link', {
+        opacity: 0, 
+        duration: 2,
+        delay: 1.2
+    })
+})
+
+document.querySelectorAll(".social-link").forEach( (link) => {
+    link.addEventListener("mouseenter", () => {
+        gsap.to(link, {
+            opacity: 1,
+        })
+    })
+
+    link.addEventListener("mouseleave", () => {
+        gsap.to(link, {
+            opacity: 0.1,
+        })
     })
 })
